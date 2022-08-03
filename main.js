cdcfdc4a53a9b61d844a190b663558b1704f8e61
vairadurai <vairadurai007@gmail.com>
2022-08-03 10:59:13 +0530

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// Increment operation using for loop
+
+let c;
+for(c=1; c<10; c++){
+    console.log(c);
+}
+// Decrement operation using for loop
+
+let d;
+for(d=10; d>0; d--){
+    console.log(d);
+}
+
+// To find 1 to 100 odd numbers
+
+let a;
+for(a=1; a<100; a++){
+    if(a%2==1){
+        console.log(a);
+    }
+}
+
+// To find 1 to 100 even numbers
+
+let b;
+for(b=1; b<100; b++){
+    if(b%2==0){
+        console.log(b);
+    }
+}

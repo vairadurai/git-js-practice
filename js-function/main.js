7720f1029f7cd59f5dc771caf9c9577779d153c7
vairadurai <vairadurai007@gmail.com>
2022-08-05 15:35:41 +0530

    add

---
+++ js-function/main.js
@@ -0,0 +1,18 @@
+
+
+
+a=4;
+b=5
+;
+function number(a,b){
+    if(a>b){
+        return a
+    }
+    if (b>a) {
+        return b
+    } else {
+        return undefined
+    }
+}
+let x=number(a,b);
+console.log(x);

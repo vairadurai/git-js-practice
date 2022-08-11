3629419da5b19dadea9a75980afbc6365be67db6
vairadurai <vairadurai007@gmail.com>
2022-08-11 14:41:33 +0530

    add

---
+++ js-mul-by-7/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Multiple by 7
+
+let n=parseInt(prompt("give a number"));
+if(n>=7){
+    console.log("number is multiple by 7");
+}
+else{
+    console.log("number is not multiple by 7");
+}

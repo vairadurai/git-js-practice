be6226e5dc252205e313c882c85983ed2f2e946d
vairadurai <vairadurai007@gmail.com>
2022-08-12 09:45:01 +0530

    add

---
+++ js-marrage-eligibility/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// Marrage Eligibility
+
+let n,b;
+let a=[];
+n=parseInt(prompt("give your age"));
+for(i=0; i<n; i++){
+    if (n<21==0) {
+        console.log("Eligibile");
+    }
+    else{
+        console.log("Not Eligibile");
+    }
+}

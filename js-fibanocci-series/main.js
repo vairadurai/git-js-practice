9895a0cfbbb667fda584300b8fba60a36d4d5d5d
vairadurai <vairadurai007@gmail.com>
2022-08-13 00:03:32 +0530

    add

---
+++ js-fibanocci-series/main.js
@@ -0,0 +1,41 @@
+
+
+
+// //fibanocci series
+
+let a,b,c,n;
+
+n=parseInt(prompt("give value"));
+a=0;
+b=1;
+console.log(a);
+console.log(b);
+for(i=0; i<n; i++){
+    c=a+b;
+    a=b;
+    b=c;
+    console.log(c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

35b977444166fe9e1017898947f3107ed7836d40
vairadurai <vairadurai007@gmail.com>
2022-08-16 12:12:12 +0530

    add

---
+++ js-quadratic-equation/main.js
@@ -0,0 +1,14 @@
+
+
+// Quadratic Equation
+
+let a,b,c,n,m;
+
+a=parseInt(prompt("give a value"));
+
+b=parseInt(prompt("give b value"));
+
+c=parseInt(prompt("give c value"));
+
+n= (-b +(Math.sqrt(b*b-4*a*c))/2*a);
+console.log(n);

f026294758951219137b955c323e86578bb19db8
vairadurai <vairadurai007@gmail.com>
2022-08-19 15:12:15 +0530

    add

---
+++ js-for-loop/main.js
@@ -0,0 +1,29 @@
+
+
+
+// get line bye line output using loop
+
+let a=["diamond","18","arun","harish"];
+for(i of a){
+    console.log(i);
+}
+
+//out put
+
+// diamond
+// 18
+// arun
+// harish
+
+//get array index values using loop
+
+let b=["a","b","c","d"];
+for(j in b){
+    console.log(j);
+}
+//out put
+
+// 0
+// 1
+// 2
+// 3

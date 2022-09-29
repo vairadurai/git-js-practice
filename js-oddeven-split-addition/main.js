00f0600ae6e46ba121dc01f1ebe0edb01d395013
vairadurai <vairadurai007@gmail.com>
2022-09-29 10:11:41 +0530

    add

---
+++ js-oddeven-split-addition/main.js
@@ -0,0 +1,29 @@
+
+
+	
+let number="12345",sum=0,result,
+even=number.replace(/[^24680]/g,""),
+odd=number.replace(/[^13579]/g,"")
+
+console.log(even);
+console.log(odd);
+sum_even=even.split("");
+//console.log(sum_even);
+
+for(i=0;i<sum_even.length;i++){
+
+    sum_even[i]=parseInt(sum_even[i]);
+    result=sum+sum_even[i];
+    sum=result;
+    
+}
+console.log(sum); 
+for(i=0;i<sum_odd.length;i++){
+
+    sum_odd[i]=parseInt(sum_odd[i]);
+    result1=sum1+sum_odd[i];
+    sum1=result1;
+    
+}
+console.log(sum1);
+

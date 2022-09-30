305c69ba28f0ff14411c9b6a6ca70d70d017bc2c
vairadurai <vairadurai007@gmail.com>
2022-09-30 14:57:55 +0530

    add-

--- js-palindrome/main.js
+++ js-palindrome/main.js
@@ -1,17 +1,49 @@
 
 
 
-// PALINDROME
+// // PALINDROME
 
-let value=prompt("give a string"),assign;
-assign=value;
-assign=assign.split("");
-assign=assign.reverse();
-assign=assign.join("");
-if(assign==value){
-    console.log("palindrome");
-}
-else{
-    console.log("not palindrome");
-}
+// let value=prompt("give a string"),assign;
+// assign=value;
+// assign=assign.split("");
+// assign=assign.reverse();
+// assign=assign.join("");
+// if(assign==value){
+//     console.log("palindrome");
+// }
+// else{
+//     console.log("not palindrome");
+// }
 
+// for (i=7; i<=200; i++){
+//     if(i%7==0){
+
+//         console.log(i);
+//         let a=[i],b=[];
+//         b=a.push(a[i]);
+//     }
+// }
+
+let object = [
+    {
+        name: "vairam",
+        age: "23",
+        hobby:" cricket"
+    },
+    {
+        name: "vairam",
+        age: "23",
+        hobby:" cricket"
+    },
+    
+    {
+        name: "vairam",
+        age: "23",
+        hobby:" cricket"
+    },
+    
+    
+];
+
+let objectlength=Object.keys(object).length;
+console.log(objectlength);
